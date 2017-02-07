a58fe4fb6299c3c85af2123cb2cfc91bf5579232
Gary Burton <gburton85@gmail.com>
2017-02-07 14:27:06 +0000

    style update

--- app/js/main.js
+++ app/js/main.js
@@ -63,3 +63,23 @@ window.onload = resize;
     }
   });
 });
+
+//accordian
+$.fn.ashCordian = function() {
+
+  var container = $(this);
+
+  container.find('header').click(function() {
+    if($(this).siblings('section').css('display') == 'block'){
+       container.find('section').slideUp(150);
+    } else {
+      container.find('section').slideUp(150);
+       $(this).siblings('section').slideDown(150);
+    }
+  });
+};
+
+
+$('#accord1').ashCordian();
+
+$('#accord2').ashCordian();
