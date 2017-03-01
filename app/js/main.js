f26d33e26498a77cfe2d7a703ace3cd2d5e1c9f8
Gary Burton <gburton85@gmail.com>
2017-03-01 12:53:20 +0000

    updated innovation layouts

--- app/js/main.js
+++ app/js/main.js
@@ -63,3 +63,23 @@ window.onload = resize;
     }
   });
 });
+
+//accordian
+$.fn.ashCordian = function() {
+
+  var container = $(this);
+
+  container.find('header').click(function() {
+    if($(this).siblings('section').css('display') == 'block'){
+       container.find('section').slideUp(150);
+    } else {
+      container.find('section').slideUp(150);
+       $(this).siblings('section').slideDown(150);
+    }
+  });
+};
+
+//accordians
+$('#accord1').ashCordian();
+
+$('#accord2').ashCordian();
